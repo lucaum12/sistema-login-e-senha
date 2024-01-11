abe0d493e71b6139827ddc4e67353dc0286ddcf4
Lucas de Souza <lucassouzarocha1202@gmail.com>
2024-01-11 16:56:08 -0300

    1 usuário e senha adicionado

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let login1 = "Lucas";
 let login2 = "João";
+let login3 = "Maria";
 let senha1 = "12defevereiro";
 let senha2 = "12demarço";
+let senha3 = "14deabril";
 let inserirLogin;
 let inserirSenha;
 
-while(inserirLogin != login1 || inserirLogin != login2){
+while(inserirLogin != login1 || inserirLogin != login2 || inserirLogin != login3){
     inserirLogin = prompt("Insira seu login:");
     if(inserirLogin == login1){
         alert(`Olá, ${login1}!`);
@@ -13,12 +15,15 @@ while(inserirLogin != login1 || inserirLogin != login2){
     }else if(inserirLogin == login2){
         alert(`Olá, ${login2}!`);
         break;
+    }else if(inserirLogin == login3){
+        alert(`Olá, ${login3}!`);
+        break;
     }else{
-        alert("Login não encontrado!");
+        alert("Usuário não encontrado, tenta novamente!")
     }
 }
 
-while(inserirSenha != senha1 || inserirSenha != senha2){
+while(inserirSenha != senha1 || inserirSenha != senha2 || inserirSenha != senha3){
     inserirSenha = prompt("Agora digite sua senha:");
     if(inserirLogin == login1){
         if(inserirSenha == senha1){
@@ -34,6 +39,14 @@ while(inserirSenha != senha1 || inserirSenha != senha2){
             break;
         }else{
             alert("Senha incorreta, tente novamente...");
+        }
+    }
+    if(inserirLogin == login3){
+        if(inserirSenha == senha3){
+            alert(`Seja bem vinda, ${login3}!`)
+            break;
+        }else{
+            alert("Senha incorreta, tente novamente...")
         }
     }
 }
